8b3806183224ef518a561cb1112e820e616d52c4
ALBERT SADYKOV <36127439+AlbertSadykovOfficial@users.noreply.github.com>
2021-04-10 18:08:10 +0300

    Add files via upload

--- Measuring_device/CONTROL.ino
+++ Measuring_device/CONTROL.ino
@@ -8,63 +8,70 @@ const char* ssid     = "ASUS";
 const char* password = "albus4082";
 
 String OBJECT = "Pipeline_1";
-String ID     = "SLAVE_LED_GADGET(Valve)";
+String ID     = "Pump";
 
+String host = "http://192.168.1.3:80";
+String reg_link = "/oil_depot/reg.php?object="+OBJECT+"&id="+ID;
 
 ESP8266WebServer httpServer(80);
 ESP8266HTTPUpdateServer httpUpdater;
 HTTPClient httpClient;
 
-void setup(void) {
-  Serial.begin(115200);
-  WiFi.mode(WIFI_AP_STA);
+void connect_LED(String value)
+{
+    if (value == "ON") { digitalWrite(2, 0); } 
+    if (value == "OFF"){ digitalWrite(2, 1); }
+}
 
-  connect_to_WIFI();
+void setup(void) 
+{
+    WiFi.mode(WIFI_AP_STA);
   
-  httpUpdater.setup(&httpServer);
-  httpServer.begin();
-  httpServer.on("/gpio_ON", handleLedOn);
-  httpServer.on("/gpio_OFF", handleLedOff);
-  
-  pinMode(2,OUTPUT);
-  pinMode(12,OUTPUT);
-  pinMode(13,OUTPUT);
-  pinMode(14,OUTPUT);
-  digitalWrite(2, 1);
-  Serial.println("setup");
+    connect_to_WIFI();
+    
+    httpUpdater.setup(&httpServer);
+    httpServer.begin();
+    httpServer.on("/gpio_ON", handleLedOn);
+    httpServer.on("/gpio_OFF", handleLedOff);
+    
+    pinMode(2,OUTPUT);
+    pinMode(12,OUTPUT);
+    pinMode(13,OUTPUT);
+    pinMode(14,OUTPUT);
 }
 
 void connect_to_WIFI()
 {
-   bool was_disconnect = false;
-   while (WiFi.status() != WL_CONNECTED) 
-  {
-    was_disconnect = true;
-    WiFi.begin(ssid, password);
-    Serial.println("trying");
-    delay(3000); // ОЧЕНЬ!!! ОЧЕНЬ !!! ОЧЕНЬ ВАЖНО!!!! Иначе все встанет и не найти ошибку 
-  }
-  if (was_disconnect)
-  {
-    registration();
-  }
+     bool was_disconnect = false;
+     
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+        was_disconnect = true;
+        connect_LED("OFF");
+        delay(3000); // Делаем паузу, чтобы не зависло
+    }
+    if (was_disconnect)
+    {
+        registration();
+    }
+    
+    connect_LED("ON");
 }
+
 void handleLedOn() { digitalWrite(14, 1); digitalWrite(12, 1); digitalWrite(13, 1); }
 void handleLedOff(){ digitalWrite(14, 0); digitalWrite(12, 0); digitalWrite(13, 0); }
+
+
 void registration()
 {
-    Serial.println("registration");
-       String host = "192.168.1.3";
-       int port = 80;
-       String link = "/new_site/reg.php?object="+OBJECT+"&id="+ID;//+"&ip="+ipToString(WiFi.localIP());
-        
-        httpClient.begin(host, port, link);
-        Serial.println(httpClient.GET());
-        httpClient.end();
+    httpClient.begin(host + reg_link);
+    httpClient.GET();
+    httpClient.end();
 }
+
 
 void loop(void) 
 {
-  connect_to_WIFI();
-  httpServer.handleClient();
+    connect_to_WIFI();
+    httpServer.handleClient();
 }
--- Measuring_device/SENSOR.ino
+++ Measuring_device/SENSOR.ino
@@ -7,95 +7,124 @@
 #include "Ultrasonic.h"
 Ultrasonic ultrasonic(5,4); 
 
-  const char* ssid     = "ASUS";
-  const char* password = "albus4082";
+const char* ssid     = "ASUS";
+const char* password = "albus4082";
 
-  String OBJECT = "RES_1";
-  String SENSOR = "UZ_1";
-   
-  String host = "192.168.1.3";
-  int port = 80;
-  String link = "/new_site/write.php?object="+OBJECT+"&sensor="+SENSOR;
+String OBJECT = "Reservoir_1";
+String SENSOR = "UZ_1";
 
-  HTTPClient http;
-  File myFile;
-  int time_now =0;
-  void setup() 
-  {
-    Serial.begin(9600);
-    WiFi.mode(WIFI_STA); // Задаем режим работы WIFI_STA (клиент)
-    WiFi.begin(ssid, password); // Подключаемся
- 
-    if (!SD.begin(4)) 
-    {
-      return;
-    }
-    time_now = millis();
-    Serial.println(time_now, millis());
-  }
-  
-  void send_data()
-  {
-    //if (millis() - time_now >= 1000)
-    //{
-        //time_now = millis();
-        //Serial.println(time_now);
-        
-        String link_rand = link + "&data=" + String(102 - 2 * ultrasonic.Ranging(CM));
-        
-        http.begin(host, port, link_rand);
-        http.GET();
-        //Serial.println(http.GET());
-        http.end();
-        delay(5000);
-    //}
-  }
-  
-  void check_connection()
-  {
+String host = "http://192.168.1.3:80";
+
+String uad_link   = host + "/oil_depot/uad.php";
+String write_link = "/oil_depot/write.php?object="+OBJECT+"&sensor="+SENSOR;
+
+
+HTTPClient http;
+File myFile;
+int time_now = 0;
+
+
+void connect_LED(String value)
+{
+    if (value == "ON") { digitalWrite(2, 0); } 
+    if (value == "OFF"){ digitalWrite(2, 1); }
+}
+
+void check_connection()
+{
     if (WiFi.status() != WL_CONNECTED)
     {
-      //bank_mode();
-      delay(1000);
+        connect_LED("OFF");
+        bank_mode();
     }
-  }
-  
-  void bank_mode()
-  {
-       String str;
-        myFile = SD.open("bank1.txt", FILE_WRITE);
-        if (myFile) 
-        {
-            while (WiFi.status() != WL_CONNECTED) // Ждем пока статус не станет WL_CONNECTED
-            {
-                if (millis() - time_now >= 5000)
-                {
-                    time_now = millis();
-                    //myFile.print("14:10:15;");
-                    myFile.print(String(102 - 2 * ultrasonic.Ranging(CM)));
-                    myFile.println(";(disconnect)");
-                }
-            }            
-        } 
-        myFile.close();
-       
-       str = "object="+OBJECT+"&sensor="+SENSOR+"&data=";
-       myFile = SD.open("bank1.txt");
-       while (myFile.available())
-       {
-         char ltr = myFile.read();
-         str += ltr;
-       }
-       myFile.close();
-       SD.remove("bank1.txt");
-       
-       http.begin("http://192.168.1.3:80/new_site/uad.php");
-       http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-       http.POST(str);
-       http.end();
-  }
-  void loop()
-  {
+    connect_LED("ON");
+}
+
+void send_GET(String link, String data)
+{
+    http.begin(link + data);
+    http.GET();
+    http.end();
+}
+
+void send_POST(String link, String data)
+{
+    http.begin(link);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    http.POST(data);
+    http.end();
+}
+
+void send_data()
+{
+    String data_link, value;
+    
+    if (millis() - time_now >= 5000)
+    {
+        time_now = millis();
+        data_link = write_link + "&data=" + get_volume();
+        send_GET(host, data_link);
+    }
+}
+
+void write_to_SD(String file)
+{
+    myFile = SD.open(file, FILE_WRITE);
+    
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        myFile.print(get_volume());
+        myFile.println(";(disconnect)");
+        delay(5000); // Делаем паузу, чтобы не зависло
+    }
+    myFile.close();
+}
+
+String read_from_SD(String file)
+{
+    String data;
+    myFile = SD.open(file);
+    
+    data = "object="+OBJECT + "&sensor="+SENSOR + "&data=";
+    while (myFile.available())
+    {
+       char ltr = myFile.read();
+       data += ltr;
+    }
+    myFile.close();
+    SD.remove(file);
+    
+    return data;
+}
+
+void bank_mode()
+{
+    String filename = "bank1.txt";
+    write_to_SD(filename);
+    send_POST(uad_link, read_from_SD(filename));
+}
+
+String get_volume()
+{
+  return String(102 - 2 * ultrasonic.Ranging(CM));
+}
+
+void setup() 
+{
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    
+    if (!SD.begin(4)) 
+    {
+        return;
+    }
+    
+    pinMode(2,OUTPUT);
+    time_now = millis();
+}
+
+void loop()
+{
     check_connection();
     send_data();
-  }
+}
