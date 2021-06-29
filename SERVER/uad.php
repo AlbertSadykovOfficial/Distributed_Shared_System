942505ec5c32610a712c13d07c624573003566d3
Albert Sadykov <albertsadykov@ro.ru>
2021-06-29 11:51:13 +0300

    brace

--- SERVER/uad.php
+++ SERVER/uad.php
@@ -1,11 +1,12 @@
 <?php
 
-if(isset($_POST['object']) && isset($_POST['sensor']) && isset($_POST['data']))
-{
-	$filename = 'storage/'.$_POST['object'].'/'.$_POST['sensor'].'.csv';
-	$filestream = fopen($filename, 'a') or die('faild');
+	if(isset($_POST['object']) && isset($_POST['sensor']) && isset($_POST['data']))
+	{
+		$filename = 'storage/'.$_POST['object'].'/'.$_POST['sensor'].'.csv';
+		$filestream = fopen($filename, 'a') or die('faild');
 
-	fwrite($filestream, $_POST['data']);
-		
-	fclose($filestream);
+		fwrite($filestream, $_POST['data']);
+			
+		fclose($filestream);
+	}
 ?>
